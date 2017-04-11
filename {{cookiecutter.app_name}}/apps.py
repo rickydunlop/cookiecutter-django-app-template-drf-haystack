bd9aef9f6544dc5afe6c3435f9f035d98e4bccc5
Ricky Dunlop <hello@rickydunlop.co.uk>
2017-04-11 14:42:03 +0100

    capitalise the app name for apps.py

--- {{cookiecutter.app_name}}/apps.py
+++ {{cookiecutter.app_name}}/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.app_name }}Config(AppConfig):
+class {{ cookiecutter.app_name.capitalize() }}Config(AppConfig):
     name = '{{ cookiecutter.app_name }}'
